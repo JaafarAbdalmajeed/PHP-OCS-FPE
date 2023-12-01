601b8c0f2763f9cec084d544633c883ac4b6b0a5
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-01 18:58:13 +0300

    task 2 in file 2

---
+++ Day 2/Tasks 2/task3.php
@@ -0,0 +1,7 @@
+<?php 
+    $cities= array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid" ); 
+foreach( $cities as $key => $value ) {
+echo "The capital of " .$key . " is." . $value;
+echo "<br>";
+}
+?>
