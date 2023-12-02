83f7f21fbb75f29551ccb8f15eb23893f4f13990
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-02 08:02:08 +0300

    task 6 in file 2

---
+++ Day 2/Tasks 2/task6.php
@@ -0,0 +1,11 @@
+<?php 
+    $fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+
+foreach ( $fruits as $key => $value) {
+echo "" . $key . " = " . $value . "";
+echo "<br>";
+}
+
+
+
+?>
