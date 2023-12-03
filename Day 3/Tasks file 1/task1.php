c74b6aba3b59b63c12d0d8a4193e8650ce4151e9
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-03 15:07:06 +0300

    msg

--- Day 3/Tasks file 1/task1.php
+++ Day 3/Tasks file 1/task1.php
@@ -5,7 +5,7 @@ Expected Output: 3 is a prime number  -->
 
 <?php 
     function primeNumber($number) {
-        for ($i = $number - 1; $i > 0; $i++) {
+        for ($i = $number - 1; $i > 1; $i++) {
             if($number % $i == 0){
                 echo " ". $number. "ia not prime number";
                 return false;
