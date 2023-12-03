ba75f475b94c0c2524c5dea0554988549c2f4b94
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-03 15:28:27 +0300

    task 4 from file 1

---
+++ Day 3/Tasks file 1/task4.php
@@ -0,0 +1,19 @@
+<!-- 4.	Write a PHP function to swap to variables?
+
+Sample Input:  x = 12     y= 10
+Expected Output: y=12   x=10  -->
+<?php 
+    $x = 12;     
+    $y= 10;
+
+    $x += $y;
+    $y = $x - $y;
+    $x -= $y
+
+    #or use other variable 
+
+
+
+?>
+
+
