4af3fd8360b5c492df2c3a341657d1ebb748fd40
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-04 08:13:25 +0300

    task 2 from file Loop

---
+++ Day 3/Loops/task2.php
@@ -0,0 +1,13 @@
+<!-- 2.	Create a script using a for loop to add all the integers between 0 and 30 and display the total. 
+
+Expected Output:  total as a number -->
+
+<?php 
+$sum = 0;
+for($i = 0; $i <= 30; $i++) {
+    $sum += $i;
+}
+
+
+
+?>
