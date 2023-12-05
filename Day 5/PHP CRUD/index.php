28e94252018e6aa3d3fd1b0a306635c9ef13ca4e
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-05 22:09:32 +0300

    msg

--- Day 5/PHP CRUD/index.php
+++ Day 5/PHP CRUD/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <div>  
@@ -20,18 +21,37 @@
         } catch (PDOException $e) {
             echo 'ERROR: '. $e->getMessage();
         }
-        $arr  = array('id', 'name', 'position', 'salary');
+        $arr  = array('id', 'name', 'position', 'salary','active');
         $len = count($arr); 
         echo '<table border="solid1px">';
-        foreach($employees as $employee) {
-            echo '<tr>';
-            for($i = 0; $i < $len; $i++) {
-                echo '<td>';
-                echo $employee[$arr[$i]];
-                echo '</td>';
+            echo '<thead>';
+                echo '<tr>';
+                    echo '<td>id</td>';
+                    echo '<td>name</td>';
+                    echo '<td>position</td>';
+                    echo '<td>salary</td>';
+                    echo '<td>active</td>';
+                echo '</tr>';
+            echo '</thead>';
+            echo '<tbody>';
+            foreach($employees as $employee) {
+                echo '<tr>';
+                for($i = 0; $i < $len; $i++) {
+                    
+                    if ($arr[$i] === 'active') {
+                        echo '<td>';
+                            echo '<a href="update.php?id=' . $employee['id'] . '"><i class="fa-solid fa-pen"></i></a>';
+                            echo '<a href="remove.php?id=' . $employee['id'] . '"><i class="fa-solid fa-trash"></i></a>';
+                        echo '</td>';
+                    } else {
+                        echo '<td>';
+                        echo $employee[$arr[$i]];
+                        echo '</td>';
+                    }
+                }
+                echo '</tr>';
             }
-            echo '</tr>';
-        }
+            echo '</tbody>';
         echo '</table>';
     ?>
 </body>
