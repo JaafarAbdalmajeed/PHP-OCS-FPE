46a874e7db501702b9679c50e3893244b68df587
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-06 08:28:58 +0300

    msg

--- Day 5/PHP CRUD/remove.php
+++ Day 5/PHP CRUD/remove.php
@@ -1,4 +1,19 @@
 <?php 
-    include 'db.php';
-    if(isset($_POST['id']))
+    
+    if(isset($_POST['id'])){
+        include 'db.php';
+
+        try {
+            $id = $_POST['id'];
+            $sql = 'DELETE FROM employees WHERE id = :id';
+            $statement = $pdo->prepare($sql);
+            if($statement->bindParam(':id', $id)){
+                echo 'success delete';
+            }
+            
+
+        } catch (PDOException $e) {
+            echo 'ERROR: '. $e->getMessage(); 
+        }
+    }
 ?>
