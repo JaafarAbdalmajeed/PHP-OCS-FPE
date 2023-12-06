d550638cda48d5aa1a1d5283343cd91d8233aa6d
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-06 14:39:49 +0300

    create welcome page and sign in

---
+++ Day 6/PHP Form Validation/Sign in/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main class="welcome">
+        <h1><strong>Hello There</strong></h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, error nam laborum blanditiis ipsam illo sapiente excepturi fugiat nemo? Provident.</p>
+        <form action="">
+            <div class="inputDiv">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+            </div>
+
+            <div class="inputDiv">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+        </form>
+        <div class="btn">
+            <a href="singIn.php" id="btnSingIn">Sign In</a>
+        </div>
+    </main>
+    
+</body>
+</html>
