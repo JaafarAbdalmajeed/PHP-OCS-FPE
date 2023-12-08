62bc285843adef981316578b3dc2cddf63cfca1d
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-08 09:33:42 +0300

    task 6

---
+++ Day 3/Loops/task6.php
@@ -0,0 +1,16 @@
+<!-- 6.	Write a program to calculate and print the factorial of a number using a for loop.
+The factorial of a number is the product of all integers up to and including that number.
+
+Sample Input: 5
+Expected Output: 120 -->
+
+<?php 
+    $number = 5;
+    $factorial = 1;
+    for ($i = $number; $i > 0; $i--) {
+        $factorial *= $i;
+    }
+    echo $factorial;
+
+
+?>
